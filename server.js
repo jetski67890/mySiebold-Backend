924b21746e830b934a84268b9d375a2d7c7a66b4
Lukas Ackermann <lukas.ackermann1@stud.tu-darmstadt.de>
2023-04-01 17:54:07 +0200

    Changed the menu plan structure and added additional mock data.

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(express.json());
 // Routes
 // Mock data for the appointments
 app.get('/appointments', (req, res) => {
-    const appointments = [
-        {
+    const appointments = [{
             id: 1,
             title: "Parent-Teacher Conference",
             date: "2023-04-10",
@@ -18,8 +17,8 @@ app.get('/appointments', (req, res) => {
             endTime: "20:00",
             location: "School Auditorium",
             description: "Discuss your child's progress and address any concerns with their teachers.",
-          },
-          {
+        },
+        {
             id: 2,
             title: "School Board Meeting",
             date: "2023-04-15",
@@ -27,8 +26,8 @@ app.get('/appointments', (req, res) => {
             endTime: "21:00",
             location: "Conference Room A",
             description: "A regular meeting of the school board to discuss various school matters and policies.",
-          },
-          {
+        },
+        {
             id: 3,
             title: "Science Fair",
             date: "2023-04-22",
@@ -36,8 +35,8 @@ app.get('/appointments', (req, res) => {
             endTime: "15:00",
             location: "School Gymnasium",
             description: "Students showcase their science projects and compete for awards and recognition.",
-          },
-          {
+        },
+        {
             id: 4,
             title: "Spring Concert",
             date: "2023-05-06",
@@ -45,8 +44,8 @@ app.get('/appointments', (req, res) => {
             endTime: "20:30",
             location: "School Auditorium",
             description: "Enjoy a night of music performed by the school's talented orchestra and choir.",
-          },
-          {
+        },
+        {
             id: 5,
             title: "Sports Day",
             date: "2023-05-20",
@@ -54,8 +53,143 @@ app.get('/appointments', (req, res) => {
             endTime: "17:00",
             location: "School Sports Ground",
             description: "A day full of athletic competitions, games, and fun activities for students and families.",
-          }
-      ];
+        },
+        {
+            id: 6,
+            title: "Book Fair",
+            date: "2023-05-28",
+            startTime: "08:00",
+            endTime: "16:00",
+            location: "School Library",
+            description: "A week-long book fair where students can purchase books and other literary resources.",
+        },
+        {
+            id: 7,
+            title: "Field Trip to the Zoo",
+            date: "2023-06-05",
+            startTime: "09:00",
+            endTime: "15:00",
+            location: "City Zoo",
+            description: "A fun and educational field trip for students to learn about different animals and their habitats.",
+        },
+        {
+            id: 8,
+            title: "Graduation Ceremony",
+            date: "2023-06-15",
+            startTime: "18:00",
+            endTime: "20:00",
+            location: "School Auditorium",
+            description: "A formal ceremony to celebrate the graduation of the senior class.",
+        },
+        {
+            id: 9,
+            title: "Open House",
+            date: "2023-08-15",
+            startTime: "08:00",
+            endTime: "12:00",
+            location: "School Campus",
+            description: "An event where prospective students and their families can tour the school campus and meet with faculty and staff.",
+        },
+        {
+            id: 10,
+            title: "Parent-Teacher Association Meeting",
+            date: "2023-09-05",
+            startTime: "19:00",
+            endTime: "21:00",
+            location: "School Conference Room",
+            description: "A meeting for parents to discuss school policies, events, and fundraising activities with the PTA.",
+        },
+        {
+            id: 11,
+            title: "Halloween Costume Contest",
+            date: "2023-10-31",
+            startTime: "14:00",
+            endTime: "16:00",
+            location: "School Gymnasium",
+            description: "A fun event for students to showcase their creative costumes and compete for prizes.",
+        },
+        {
+            id: 12,
+            title: "Thanksgiving Potluck",
+            date: "2023-11-22",
+            startTime: "12:00",
+            endTime: "14:00",
+            location: "School Cafeteria",
+            description: "A potluck lunch for students and staff to celebrate Thanksgiving and share their favorite dishes.",
+        },
+        {
+            id: 13,
+            title: "Winter Break",
+            date: "2023-12-22",
+            startTime: "",
+            endTime: "",
+            location: "",
+            description: "School will be closed for winter break from December 22, 2023 to January 2, 2024.",
+        },
+        {
+            id: 14,
+            title: "Winter Concert",
+            date: "2024-01-12",
+            startTime: "18:30",
+            endTime: "20:30",
+            location: "School Auditorium",
+            description: "Enjoy a night of music performed by the school's talented orchestra and choir.",
+        },
+        {
+            id: 15,
+            title: "Math Competition",
+            date: "2024-02-14",
+            startTime: "09:00",
+            endTime: "12:00",
+            location: "School Classroom",
+            description: "A competition for students to showcase their math skills and compete for prizes.",
+        },
+        {
+            id: 16,
+            title: "Career Day",
+            date: "2024-03-05",
+            startTime: "09:00",
+            endTime: "12:00",
+            location: "School Auditorium",
+            description: "A day for students to learn about different careers and meet with professionals from various industries.",
+        },
+        {
+            id: 17,
+            title: "Spring Break",
+            date: "2024-03-25",
+            startTime: "",
+            endTime: "",
+            location: "",
+            description: "School will be closed for spring break from March 25, 2024 to April 1, 2024.",
+        },
+        {
+            id: 18,
+            title: "Art Exhibition",
+            date: "2024-04-18",
+            startTime: "16:00",
+            endTime: "19:00",
+            location: "School Art Gallery",
+            description: "An exhibition featuring artwork created by students in various art classes.",
+        },
+        {
+            id: 19,
+            title: "Drama Production",
+            date: "2024-05-10",
+            startTime: "18:30",
+            endTime: "20:30",
+            location: "School Auditorium",
+            description: "A production of a popular play or musical performed by the school's drama department.",
+        },
+        {
+            id: 20,
+            title: "End of Year Picnic",
+            date: "2024-06-07",
+            startTime: "11:00",
+            endTime: "14:00",
+            location: "School Sports Ground",
+            description: "A fun outdoor event for students and staff to celebrate the end of the school year with games, food, and music.",
+        }
+    ];
     res.json(appointments);
   });
 
@@ -489,78 +623,91 @@ app.get('/plan', (req, res) => {
 app.get('/menu', (req, res) => {
     const menu = [
         {
-            "2023-04-03": {
-              breakfast: {
-                main: "Scrambled eggs",
-                side: "Toast with butter",
-                beverage: "Orange juice"
-              },
-              lunch: {
-                main: "Grilled chicken sandwich",
-                side: "Mixed green salad",
-                dessert: "Apple slices",
-                beverage: "Water"
-              },
-              snack: "Yogurt with granola"
+            id: "1",
+            date: "2023-04-01",
+            breakfast: {
+                main: "Scrambled Eggs",
+                side: "Hash Browns",
+                dessert: "Blueberry Muffin",
+                beverage: "Orange Juice"
             },
-            "2023-04-04": {
-              breakfast: {
-                main: "Pancakes with syrup",
-                side: "Fresh fruit",
+            lunch: {
+                main: "Grilled Chicken Sandwich",
+                side: "Sweet Potato Fries",
+                dessert: "Chocolate Chip Cookie",
+                beverage: "Iced Tea"
+            },
+            snack: "Trail Mix"
+        },
+        {
+            id: "2",
+            date: "2023-04-02",
+            breakfast: {
+                main: "French Toast",
+                side: "Sausage Links",
+                dessert: "Banana",
                 beverage: "Milk"
-              },
-              lunch: {
-                main: "Spaghetti with marinara sauce",
-                side: "Garlic bread",
-                dessert: "Peach cup",
-                beverage: "Water"
-              },
-              snack: "Cheese and crackers"
             },
-            "2023-04-05": {
-              breakfast: {
-                main: "Oatmeal with raisins",
-                side: "Toast with jam",
-                beverage: "Apple juice"
-              },
-              lunch: {
-                main: "Turkey and cheese wrap",
-                side: "Carrot sticks",
-                dessert: "Chocolate chip cookie",
-                beverage: "Water"
-              },
-              snack: "Veggie sticks with hummus"
-            },
-            "2023-04-06": {
-              breakfast: {
-                main: "Breakfast burrito",
-                side: "Fresh fruit",
+            lunch: {
+                main: "Chicken Nuggets",
+                side: "Macaroni and Cheese",
+                dessert: "Apple",
                 beverage: "Milk"
-              },
-              lunch: {
-                main: "Cheese pizza",
-                side: "Caesar salad",
-                dessert: "Orange slices",
-                beverage: "Water"
-              },
-              snack: "Pretzels with mustard"
             },
-            "2023-04-07": {
-              breakfast: {
-                main: "Cereal with milk",
+            snack: "Apple"
+        },
+        {
+            id: "3",
+            date: "2023-04-03",
+            breakfast: {
+              main: "Pancakes",
+              side: "Bacon",
+              dessert: "Strawberries",
+              beverage: "Coffee"
+            },
+            lunch: {
+              main: "Turkey Sandwich",
+              side: "Chips",
+              dessert: "Grapes",
+              beverage: "Water"
+            },
+            snack: "Popcorn"
+        },
+        {
+            id: "4",
+            date: "2023-04-04",
+            breakfast: {
+                main: "Oatmeal",
                 side: "Banana",
-                beverage: "Orange juice"
-              },
-              lunch: {
-                main: "Fish sticks",
-                side: "Steamed mixed vegetables",
-                dessert: "Apple slices",
+                dessert: "Apple",
+                beverage: "Milk"
+            },
+            lunch: {
+                main: "Pizza",
+                side: "Carrots",
+                dessert: "Chocolate Chip Cookie",
                 beverage: "Water"
-              },
-              snack: "Popcorn"
-            }
-          }        
-      ];
+            },
+            snack: "Fruit slices"
+        },
+        {
+            id: "5",
+            date: "2023-04-05",
+            breakfast: {
+                main: "Waffles",
+                side: "Fries",
+                dessert: "Ice cream",
+                beverage: "Milk"
+            },
+            lunch: {
+                main: "Mozzarella Sticks",
+                side: "Tomato Soup",
+                dessert: "Apple",
+                beverage: "Water"
+            },
+            snack: "Apple"
+        }
+    ];
       res.json(menu);
 });
   
